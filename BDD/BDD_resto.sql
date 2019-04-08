c43d5e286c74d0c5222fe6a1e3d1c48c773dcbb6
JeanMahmoudDavid <nathan.moresco@icloud.com>
2019-04-08 08:47:11 +0200

    bddencore

--- BDD/BDD_resto.sql
+++ BDD/BDD_resto.sql
@@ -85,32 +85,6 @@ INSERT INTO Commande (dateCommande) VALUES
 ("2018-07-26"),
 ("2018-07-26");
 
-INSERT INTO Groupe (numeroTable) VALUES
-(1),
-(2),
-(3),
-(4),
-(5),
-(6),
-(7),
-(8),
-(9),
-(10),
-(11),
-(12),
-(13),
-(14),
-(15),
-(16),
-(17),
-(18),
-(19),
-(20),
-(21),
-(22),
-(23),
-(24);
-
 INSERT INTO Categorie (libelle) VALUES
 ("Poulettito"),
 ("Legumes de mer"),
